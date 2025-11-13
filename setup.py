9d299df02b16c884cd43820da5423d3172ae6680
Hariom_Nagar <bhandarihariom211@gmail.com>
2025-11-14 01:58:32 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Hariom Nagar",
+    author_email="bhandarihariom211@gmail.com",
+    packages=find_packages()
+)
